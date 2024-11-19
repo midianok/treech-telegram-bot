6da66021f2b604b54a62a711807ec10223dca319
Ilya Gusev <midian.ip@gmail.com>
2024-11-19 19:09:41 +0300

    refactor

--- Saturn.Telegram.Service/Program.cs
+++ Saturn.Telegram.Service/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using Saturn.Bot.Service;
 using Saturn.Bot.Service.Database;
+using Saturn.Bot.Service.Extension;
 using Saturn.Bot.Service.Operations;
 using Saturn.Bot.Service.Operations.Abstractions;
 using Saturn.Bot.Service.Options;
@@ -17,33 +18,19 @@ builder.Services.AddOptions<OperationOptions>()
 
 builder.Services.AddDbContextFactory<SaturnContext>(options =>
 {
-    var connectionString = builder.Configuration.GetSection("CONNECTION_STRING").Value;
-    if (string.IsNullOrWhiteSpace(connectionString))
-    {
-        throw new Exception("Env variable CONNECTION_STRING not presented");
-    }
-    options.UseNpgsql(connectionString)
-        .UseSnakeCaseNamingConvention();
+    var connectionString = builder.Configuration.GetSectionOrThrow("CONNECTION_STRING");
+    options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
 }, ServiceLifetime.Transient);
-
 
 builder.Services.AddSingleton<TelegramBotClient>(_ =>
 {
-    var botToken = builder.Configuration.GetSection("BOT_TOKEN").Value;
-    if (string.IsNullOrWhiteSpace(botToken))
-    {
-        throw new Exception("Env variable BOT_TOKEN not presented");
-    }
+    var botToken = builder.Configuration.GetSectionOrThrow("BOT_TOKEN");
     return new TelegramBotClient(botToken);
 });
 
 builder.Services.AddHttpClient<IImageManipulationServiceClient, ImageManipulationServiceClient>(x =>
 {
-    var imageManipulationServiceUrl = builder.Configuration.GetSection("IMAGE_MANIPULATION_SERVICE_URL").Value;
-    if (string.IsNullOrWhiteSpace(imageManipulationServiceUrl))
-    {
-        throw new Exception("Env variable ImageManipulationServiceUrl not presented");
-    }
+    var imageManipulationServiceUrl = builder.Configuration.GetSectionOrThrow("IMAGE_MANIPULATION_SERVICE_URL");
     x.BaseAddress = new Uri(imageManipulationServiceUrl);
 });
 
