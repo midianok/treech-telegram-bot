5d7aa624ac4d177913443a6f02cd02503b2b4054
Ilya Gusev <midian.ip@gmail.com>
2024-11-19 19:09:41 +0300

    refactor

---
+++ Saturn.Telegram.Service/Extension/Extension.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.Configuration;
+
+namespace Saturn.Bot.Service.Extension;
+
+public static class Extension
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions =
+        new() { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault };
+
+    public static string ToJson<T>(this T obj) where T : class =>
+        JsonSerializer.Serialize(obj, _jsonSerializerOptions);
+
+    public static string GetSectionOrThrow(this IConfiguration configuration, string key)
+    {
+        var item = configuration.GetSection(key).Value;
+        if (string.IsNullOrWhiteSpace(item))
+        {
+            throw new Exception($"configuration item {key} not presented");
+        }
+
+        return item;
+    }
+}
--- Saturn.Telegram.Service/HostedService.cs
+++ Saturn.Telegram.Service/HostedService.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Saturn.Bot.Service.Database;
+using Saturn.Bot.Service.Extension;
 using Saturn.Bot.Service.Operations.Abstractions;
 using Telegram.Bot;
 
@@ -9,42 +10,31 @@ namespace Saturn.Bot.Service;
 
 public class HostedService : IHostedService
 {
-    private readonly CancellationTokenSource _cancellationTokenSource;
+    private readonly TelegramBotClient _telegramBotClient;
     private readonly IEnumerable<IOperation> _operations;
     private readonly IDbContextFactory<SaturnContext> _contextFactory;
-    private readonly IConfiguration _configuration;
 
-    public HostedService(IEnumerable<IOperation> operations, IConfiguration configuration, IDbContextFactory<SaturnContext> contextFactory)
+    public HostedService(TelegramBotClient telegramBotClient, IEnumerable<IOperation> operations, IDbContextFactory<SaturnContext> contextFactory)
     {
         _contextFactory = contextFactory;
-        _cancellationTokenSource = new CancellationTokenSource();
         _operations = operations;
-        _configuration = configuration;
+        _telegramBotClient = telegramBotClient;
     }
 
     public async Task StartAsync(CancellationToken cancellationToken)
     {
-        var botToken = _configuration.GetSection("BOT_TOKEN").Value;
-        if (string.IsNullOrWhiteSpace(botToken))
-        {
-            throw new Exception("Env variable BOT_TOKEN not presented");
-        }
-        var bot = new TelegramBotClient(botToken, cancellationToken: _cancellationTokenSource.Token);
-
         var db = await _contextFactory.CreateDbContextAsync(cancellationToken);
         await db.Database.EnsureCreatedAsync(cancellationToken);
         await db.Database.MigrateAsync(cancellationToken);
 
         foreach (var operation in _operations)
         {
-            bot.OnError += operation.OnErrorAsync;
-            bot.OnMessage += operation.OnMessageAsync;
-            bot.OnUpdate += operation.OnUpdateAsync;
+            _telegramBotClient.OnError += operation.OnErrorAsync;
+            _telegramBotClient.OnMessage += operation.OnMessageAsync;
+            _telegramBotClient.OnUpdate += operation.OnUpdateAsync;
         }
     }
 
-    public Task StopAsync(CancellationToken cancellationToken)
-    {
-        return _cancellationTokenSource.CancelAsync();
-    }
+    public Task StopAsync(CancellationToken cancellationToken) =>
+        Task.CompletedTask;
 }
