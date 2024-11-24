a5da329b3466b7fca8c68c9121ff6dad5ab623e2
Ilya Gusev <midian.ip@gmail.com>
2024-11-24 15:34:29 +0300

    топ стата

--- Saturn.Telegram.Service/Operations/CountOperation.cs
+++ Saturn.Telegram.Service/Operations/CountOperation.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Text;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Saturn.Bot.Service.Database;
 using Saturn.Bot.Service.Extension;
@@ -29,7 +30,49 @@ public class CountOperation : OperationBase
         await SaveMessage(msg, db);
         await ShowStat(msg, type, db);
         await ShowFavStick(msg, type, db);
+        await ShowTop(msg, type, db);
     }
+
+    private async Task ShowTop(Message msg, UpdateType type, SaturnContext db)
+    {
+        var match = type == UpdateType.Message && msg.Text?.ToLower() == "топ стата";
+        if (!match)
+        {
+            return;
+        }
+
+        var monday = GetMondayDate();
+
+        var topUsersByMessageCount = await db.Messages.Where(x => x.ChatId == msg.Chat.Id && x.MessageDate > monday && x.MessageDate < DateTime.Now)
+            .Where(x => x.FromUserId.HasValue)
+            .GroupBy(x => x.FromUserId)
+            .Select(x => new { UserId = x.Key, UserName = x.First().FromUsername, FirstName = x.First().FromFirstName, LastName = x.First().FromLastName, MessageCount = x.Count()})
+            .OrderByDescending(x => x.MessageCount)
+            .Take(10).ToListAsync();
+
+        var replyMessage = new StringBuilder("Топ за неделю по сообщениям:\n");
+        var iterator = 1;
+
+        foreach (var user in topUsersByMessageCount)
+        {
+            var userName = !string.IsNullOrEmpty(user.UserName) ? user.UserName : user.UserId.ToString();
+
+            var emoji = GetEmoji(iterator++);
+            replyMessage.Append($"{emoji} {user.FirstName} {user.LastName} (@{userName}): {user.MessageCount}\n");
+        }
+
+        await _telegramBotClient.SendMessage(msg.Chat, replyMessage.ToString(), ParseMode.None,
+            new ReplyParameters { MessageId = msg.Id } );
+    }
+
+    private string GetEmoji(int iterator) =>
+        iterator switch
+        {
+            1 => "🥇",
+            2 => "🥈",
+            3 => "🥉",
+            _ => string.Empty
+        };
 
     private async Task ShowFavStick(Message msg, UpdateType type, SaturnContext db)
     {
@@ -106,4 +149,17 @@ public class CountOperation : OperationBase
         });
         await db.SaveChangesAsync();
     }
+
+    private DateTime GetMondayDate() =>
+        DateTime.Now.DayOfWeek switch
+        {
+            DayOfWeek.Monday => DateTime.Now.Date,
+            DayOfWeek.Tuesday => DateTime.Now.AddDays(-1).Date,
+            DayOfWeek.Wednesday => DateTime.Now.AddDays(-2).Date,
+            DayOfWeek.Thursday => DateTime.Now.AddDays(-3).Date,
+            DayOfWeek.Friday => DateTime.Now.AddDays(-4).Date,
+            DayOfWeek.Saturday => DateTime.Now.AddDays(-5).Date,
+            DayOfWeek.Sunday => DateTime.Now.AddDays(-6).Date,
+            _ => throw new ArgumentOutOfRangeException()
+        };
 }
