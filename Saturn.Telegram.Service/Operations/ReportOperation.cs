7d5dba2889a913e9de9e2e36b3c57591e237308e
Ilya Gusev <midian.ip@gmail.com>
2025-04-06 11:13:05 +0700

    UploadOperation ReportOperation

---
+++ Saturn.Telegram.Service/Operations/ReportOperation.cs
@@ -0,0 +1,27 @@
+﻿using Saturn.Telegram.Lib.Operation;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Saturn.Bot.Service.Operations;
+
+public class ReportOperation : OperationBase
+{
+    private const long ReportChatId = -1001864826490;
+    protected override async Task ProcessOnMessageAsync(Message msg, UpdateType type)
+    {
+        await TelegramBotClient.SendMessage(ReportChatId,
+            "REPORT: @nkess @qwrzlp @trofimovng @CptDragynov @ilya\\_naprimer",
+            ParseMode.MarkdownV2,
+            replyMarkup: InlineKeyboardButton.WithUrl("MESSAGE", $"https://t.me/c/1536029449/{msg.ReplyToMessage!.MessageId}"));
+        
+        await TelegramBotClient.SendMessage(msg.Chat, "Отчёт отправлен");
+    }  
+
+    protected override bool ValidateOnMessage(Message msg, UpdateType type) =>
+        type == UpdateType.Message &&
+        !string.IsNullOrEmpty(msg.Text) &&
+        msg.ReplyToMessage != null &&
+        msg.Text.ToLower().StartsWith("/report");
+}
---
+++ Saturn.Telegram.Service/Operations/UploadOperation.cs
@@ -0,0 +1,38 @@
+﻿using Saturn.Telegram.Lib.Operation;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Saturn.Bot.Service.Operations;
+
+public class UploadOperation : OperationBase
+{
+    protected override async Task ProcessOnMessageAsync(Message msg, UpdateType type)
+    {
+        var fileId = msg.ReplyToMessage!.Photo!.MaxBy(x => x.FileSize)!.FileId;
+        if (string.IsNullOrEmpty(fileId))
+        {
+            return;
+        }
+
+        var file = await TelegramBotClient.GetFile(fileId);
+        if (string.IsNullOrEmpty(file.FilePath))
+        {
+            return;
+        }
+        using var downloadFileStream = new MemoryStream();
+        await TelegramBotClient.DownloadFile(file.FilePath, downloadFileStream);
+        var fileName = Guid.NewGuid().ToString()[..8];
+        var filePath = $"/app/files/{fileName}.jpg";
+        
+        await File.WriteAllBytesAsync(filePath, downloadFileStream.ToArray());
+        
+        await TelegramBotClient.SendMessage(msg.Chat, $"https://treech.ru/st/{fileName}.jpg");
+    }  
+
+    protected override bool ValidateOnMessage(Message msg, UpdateType type) =>
+        type == UpdateType.Message &&
+        !string.IsNullOrEmpty(msg.Text) &&
+        msg.ReplyToMessage?.Photo != null &&
+        msg.Text.ToLower() == "загрузи";
+}
