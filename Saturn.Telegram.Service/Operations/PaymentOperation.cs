60d9249440aba7716da7c9441e6b36237d466f4e
Ilya Gusev <midian.ip@gmail.com>
2025-06-15 23:22:24 +0300

    refactor: streamline `OperationBase` validation and processing methods

---
+++ Saturn.Telegram.Service/Operations/PaymentOperation.cs
@@ -0,0 +1,46 @@
+﻿using Saturn.Telegram.Lib.Operation;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Saturn.Bot.Service.Operations;
+
+public class PaymentOperation : OperationBase
+{
+    protected override Task ProcessOnMessageAsync(Message msg, UpdateType type)
+    {
+        return type switch
+        {
+            UpdateType.Message when msg.Text!.StartsWith("/payment") => Process(msg, type)
+        };
+    }
+
+    private async Task Process(Message msg, UpdateType type)
+    {
+        var chatId = msg.Chat.Id;
+        var keyboard = new InlineKeyboardMarkup( new[]
+        {
+            InlineKeyboardButton.WithCallbackData("На неделю", $"ch_gn_7_{chatId}"),
+            InlineKeyboardButton.WithCallbackData("На на месяц", $"ch_gn_31_{chatId}"),
+            
+        });
+        
+        await TelegramBotClient.SendMessage(msg.Chat, "Платные подписки", ParseMode.MarkdownV2, replyMarkup: keyboard);
+    }
+    protected override Task ProcessOnUpdateAsync(Update update)
+    {
+        return update.Type switch
+        {
+            UpdateType.CallbackQuery when update.CallbackQuery!.Message.Chat.Type == ChatType.Private && update.CallbackQuery!.Data.StartsWith("ch_gn_7_") => ProcessUpdate(update)
+        };
+    }
+
+    private async Task ProcessUpdate(Update update)
+    {
+        //await TelegramBotClient.SendMessage(msg.Chat, "Отдохни ещё чутка", ParseMode.MarkdownV2, replyMarkup: InlineKeyboardButton.WithUrl("Убрать задержку на неделю", t));
+        await TelegramBotClient.EditMessageText(update.CallbackQuery!.Message.Chat, update.CallbackQuery!.Message.Id, "Платные подписки", ParseMode.MarkdownV2, replyMarkup: InlineKeyboardButton.WithCallbackData("На на месяц", $"ch_gn_31_"));
+    }
+
+    protected override bool ValidateOnTextMessage(Message msg, UpdateType type) => true;
+}
---
+++ Saturn.Telegram.Service/Operations/SendInvoiceOperation.cs
@@ -0,0 +1,38 @@
+﻿using Saturn.Telegram.Lib.Operation;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Saturn.Bot.Service.Operations;
+
+public class SendInvoiceOperation : OperationBase
+{
+    protected override async Task ProcessOnMessageAsync(Message msg, UpdateType type)
+    {
+        // await TelegramBotClient.SendMessage(msg.Chat, 
+        //     "Отдохни ещё чутка", 
+        //     ParseMode.MarkdownV2, 
+        //     replyMarkup: InlineKeyboardButton.WithCallbackData("Убрать задержку на неделю", "qwqwqweqwe"));
+        //
+        //var t = await TelegramBotClient.CreateInvoiceLink("Отдохни ещё", "Отдохни ещё", $"testset_{msg.Chat.Id}", "XTR", [new LabeledPrice("Убрать задержку на неделю", 1)]);
+        //await TelegramBotClient.SendMessage(msg.Chat, "Отдохни ещё чутка", ParseMode.MarkdownV2, replyMarkup: InlineKeyboardButton.WithUrl("Убрать задержку на неделю", t));
+        //await TelegramBotClient.SendInvoice(msg.Chat.Id, "Отдохни ещё", "Отдохни ещё", $"ChatGeneration_{msg.Chat.Id}", "XTR", [ new LabeledPrice("Убрать задержку на неделю", 1) ]);
+    }
+
+    protected override async Task ProcessOnUpdateAsync(Update update)
+    {
+        //await TelegramBotClient.SendMessage(update.Message.Chat, "пук");
+        
+        // switch (update.Type)
+        // {
+        //     case UpdateType.PreCheckoutQuery:
+        //         await TelegramBotClient.AnswerPreCheckoutQuery(update.PreCheckoutQuery.Id);
+        //         break;
+        //     default:
+        //         throw new ArgumentOutOfRangeException();
+        // }
+    }
+
+    protected override bool ValidateOnTextMessage(Message msg, UpdateType type) => false;
+}
