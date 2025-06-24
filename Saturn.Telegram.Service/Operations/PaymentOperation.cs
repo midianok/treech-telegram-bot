3b0e5a258e3df97a7cb4abcaa8289fbb81439216
Ilya Gusev <midian.ip@gmail.com>
2025-06-24 22:39:17 +0300

    refactor: enhance payment and cooldown processing, add logging

--- Saturn.Telegram.Service/Operations/PaymentOperation.cs
+++ Saturn.Telegram.Service/Operations/PaymentOperation.cs
@@ -2,45 +2,49 @@
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.Payments;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace Saturn.Bot.Service.Operations;
 
 public class PaymentOperation : OperationBase
 {
-    protected override Task ProcessOnMessageAsync(Message msg, UpdateType type)
+    protected override async Task ProcessOnMessageAsync(Message msg, UpdateType type)
     {
-        return type switch
-        {
-            UpdateType.Message when msg.Text!.StartsWith("/payment") => Process(msg, type)
-        };
-    }
-
-    private async Task Process(Message msg, UpdateType type)
-    {
-        var chatId = msg.Chat.Id;
-        var keyboard = new InlineKeyboardMarkup( new[]
-        {
-            InlineKeyboardButton.WithCallbackData("На неделю", $"ch_gn_7_{chatId}"),
-            InlineKeyboardButton.WithCallbackData("На на месяц", $"ch_gn_31_{chatId}"),
-            
-        });
+        var keyboard = new InlineKeyboardMarkup(InlineKeyboardButton.WithCallbackData("На 7 дней ", "sub7"), InlineKeyboardButton.WithCallbackData("На 30 дней", "sub30"));
         
         await TelegramBotClient.SendMessage(msg.Chat, "Платные подписки", ParseMode.MarkdownV2, replyMarkup: keyboard);
     }
+    
     protected override Task ProcessOnUpdateAsync(Update update)
     {
         return update.Type switch
         {
-            UpdateType.CallbackQuery when update.CallbackQuery!.Message.Chat.Type == ChatType.Private && update.CallbackQuery!.Data.StartsWith("ch_gn_7_") => ProcessUpdate(update)
+            UpdateType.CallbackQuery when update.CallbackQuery!.Message!.Chat.Type == ChatType.Private && update.CallbackQuery!.Data!.StartsWith("sub") => SendInvoice(update),
+            UpdateType.PreCheckoutQuery => ProcessPayment(update)
         };
     }
 
-    private async Task ProcessUpdate(Update update)
+    private async Task SendInvoice(Update update)
     {
-        //await TelegramBotClient.SendMessage(msg.Chat, "Отдохни ещё чутка", ParseMode.MarkdownV2, replyMarkup: InlineKeyboardButton.WithUrl("Убрать задержку на неделю", t));
-        await TelegramBotClient.EditMessageText(update.CallbackQuery!.Message.Chat, update.CallbackQuery!.Message.Id, "Платные подписки", ParseMode.MarkdownV2, replyMarkup: InlineKeyboardButton.WithCallbackData("На на месяц", $"ch_gn_31_"));
+        var days = update.CallbackQuery!.Data switch
+        {
+            "sub7" => "7",
+            "sub30" => "30"
+        };
+        
+        await TelegramBotClient.SendInvoice(update.CallbackQuery!.Message!.Chat, "Оплата подписки", $"Убрать задержку на {days} дней", update.CallbackQuery!.Data, "XTR", [new LabeledPrice($"Убрать задержку!!!!! на {days} дней", 1)]);
+        
+        await TelegramBotClient.AnswerCallbackQuery(update.CallbackQuery.Id);
+    }
+    
+    private async Task ProcessPayment(Update update)
+    {
+        //todo:обработка подписки
+        await TelegramBotClient.AnswerPreCheckoutQuery(update.PreCheckoutQuery!.Id);
     }
 
-    protected override bool ValidateOnTextMessage(Message msg, UpdateType type) => true;
+    protected override bool ValidateOnTextMessage(Message msg, UpdateType type) => 
+        msg.Text == "/start payment";
 }
