652e8c90665e9b16c1d2f7a9f70fd712c02f4503
Ilya Gusev <midian.ip@gmail.com>
2025-07-14 15:33:50 +0300

    migration: add `AiAgents` table, extend `messages` table format

---
+++ Saturn.Telegram.Db/Migrations/20250713120550_AiAgents.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Saturn.Telegram.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AiAgents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "text",
+                table: "messages",
+                type: "character varying(4096)",
+                maxLength: 4096,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "sticker_id",
+                table: "messages",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "reply_to_message_chat_id",
+                table: "messages",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "reply_to_message_id",
+                table: "messages",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ai_agents",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    code = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    prompt = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_ai_agents", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ai_agents");
+
+            migrationBuilder.DropColumn(
+                name: "reply_to_message_chat_id",
+                table: "messages");
+
+            migrationBuilder.DropColumn(
+                name: "reply_to_message_id",
+                table: "messages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "text",
+                table: "messages",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(4096)",
+                oldMaxLength: 4096,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "sticker_id",
+                table: "messages",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+        }
+    }
+}
