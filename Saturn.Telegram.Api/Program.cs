5cb95660b61c1985ebef8156700ef472bb676e5c
Ilya Gusev <midian.ip@gmail.com>
2026-04-12 19:30:57 +0700

    Add Saturn.Telegram.Api project: initialize Web API with support for Swagger, CORS, and weekly stats endpoint.

---
+++ Saturn.Telegram.Api/Program.cs
@@ -0,0 +1,34 @@
+using Saturn.Telegram.Db.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddSaturnContext(builder.Configuration);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+        policy.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
