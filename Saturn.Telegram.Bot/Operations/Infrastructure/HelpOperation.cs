c07aa67528a96154ed92c6c70b501589f9fcc9d5
Ilya Gusev <midian.ip@gmail.com>
2026-04-23 13:21:03 +0700

    Add: `HelpOperation` to serve bot usage guide and include `help.md` in output directory

---
+++ Saturn.Telegram.Bot/Operations/Infrastructure/HelpOperation.cs
@@ -0,0 +1,23 @@
+using Saturn.Bot.Service.Extensions;
+using Saturn.Telegram.Lib.Operation;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Saturn.Bot.Service.Operations.Infrastructure;
+
+public class HelpOperation(TelegramBotClient telegramBotClient) : IOperation
+{
+    private readonly string _helpText = File
+        .ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "help.md"))
+        .EscapeMarkdownV2();
+
+    public bool Validate(Message msg, UpdateType type) =>
+        !string.IsNullOrEmpty(msg.Text) && msg.Text.ToLower() == "помощь";
+
+    public Task OnMessageAsync(Message msg, UpdateType type) =>
+        telegramBotClient.SendMessage(msg.Chat, _helpText, ParseMode.MarkdownV2,
+            new ReplyParameters { MessageId = msg.Id });
+
+    public Task OnUpdateAsync(Update update) => Task.CompletedTask;
+}
