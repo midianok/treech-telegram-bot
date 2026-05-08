0aeaf4a3d5168ce92b97c5a8e046248af8e383bb
Ilya Gusev <midian.ip@gmail.com>
2026-05-08 21:37:57 +0700

    Add: Introduce `NamorevoGoreOperation` and update help documentation

---
+++ Saturn.Telegram.Bot/Operations/FunnyStaff/NamorevoGoreOperation.cs
@@ -0,0 +1,20 @@
+using Saturn.Telegram.Lib.Operation;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Saturn.Bot.Service.Operations.FunnyStaff;
+
+public class NamorevoGoreOperation(TelegramBotClient telegramBotClient) : IOperation
+{
+    public bool Validate(Message msg, UpdateType type) =>
+        !string.IsNullOrEmpty(msg.Text) &&
+        msg.Text.Contains("наморево горе", StringComparison.OrdinalIgnoreCase);
+
+    public Task OnMessageAsync(Message msg, UpdateType type)
+    {
+        var keyboard = new InlineKeyboardMarkup(InlineKeyboardButton.WithUrl("Наморево горе", "https://t.me/TreechBot/namorevogore"));
+        return telegramBotClient.SendMessage(msg.Chat, "Наморево горе!", ParseMode.None, new ReplyParameters { MessageId = msg.Id }, replyMarkup: keyboard);
+    }
+}
