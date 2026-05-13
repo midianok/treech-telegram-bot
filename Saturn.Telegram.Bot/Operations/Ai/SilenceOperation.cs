6635f75d485328c98229c502ee7fb57ff166f251
Ilya Gusev <midian.ip@gmail.com>
2026-05-13 17:37:35 +0700

    Update: Simplify prompt message in SilenceOperation to improve clarity

--- Saturn.Telegram.Bot/Operations/Ai/SilenceOperation.cs
+++ Saturn.Telegram.Bot/Operations/Ai/SilenceOperation.cs
@@ -10,7 +10,7 @@ namespace Saturn.Bot.Service.Operations.Ai;
 
 public class SilenceOperation : IOperation
 {
-    private const string Prompt = "В чате долго молчат. Расскажи какойю-нибудь историю или тегни Username человека которого ты знаешь и скажи ему что-нибудь";
+    private const string Prompt = "В чате долго молчат. Расскажи какойю-нибудь историю про человека которого ты знаешь";
 
     private const int MinSilenceMinutes = 10;
     private const int MaxSilenceMinutes = 40;
